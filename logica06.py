adc4f8effb58cda94d5462068dd0f0ccba88156e
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-05-18 12:10:42 -0300

    desenvolvimentio python logica de programaçãao

---
+++ logica06.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite o numero: '))
+
+count = 1
+
+while(count <= 10):
+     resultado = numero * count
+     print(f'{numero} X {count} = {resultado}')
+     count = count + 1
